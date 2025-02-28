c75238c00cb16a65b972510b92012e572022e3a9
Valentin developer <buziza@yandex.ru>
2025-02-28 17:51:45 +0300

    dev_2025_02_28

--- Project/Diploma/user-security/src/main/java/security_config/SecurityConfig.java
+++ Project/Diploma/user-security/src/main/java/security_config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -17,12 +18,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorizeHttpRequests) ->
+                .authorizeHttpRequests( (authorizeHttpRequests) ->
                         authorizeHttpRequests
-                                .requestMatchers("/**").hasRole("USER")
-                                .anyRequest().authenticated()
+                                .requestMatchers( "/**" )
+                                .permitAll()
+                                //.hasRole("USER")
+                                .anyRequest()
+                                .authenticated()
                 )
-                .formLogin(withDefaults());
+                .formLogin( withDefaults() );
+        http.headers( headers -> headers
+                .frameOptions( HeadersConfigurer.FrameOptionsConfig::sameOrigin ) );
         return http.build();
 
     }
